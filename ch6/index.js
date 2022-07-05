5d1b0046985076eb54b2767d7972c5be6c7297d0
Kyu0 <efftar@naver.com>
2022-07-05 18:26:03 +0900

    delete 요청 추가

--- ch6/index.js
+++ ch6/index.js
@@ -40,6 +40,14 @@ app.put('/api/tours/:id', (req, res) => {
     res.json({ success: true })
 })
 
+app.delete('/api/tour/:id', (req, res) => {
+    const idx = tours.findIndex(tour => tour.id === parseInt(req.params.id))
+
+    if (idx < 0) return res.json({ error: 'No such tour exists' })
+    tours.splice(idx, 1)
+    res.json({ success: true })
+})
+
 app.listen(port, () => {
     console.log(`Express started on port number : ${port}`)
 })

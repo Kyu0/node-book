388e3949c78e9299fdb64eafa8393c7f15ead546
Kyu0 <efftar@naver.com>
2022-07-05 15:11:27 +0900

    퍼펫티어 적용 전 사전작업 node.js 로 main javascript 파일을 실행한 경우와 아닌 경우(테스트 모듈 사용 등 다른 스크립트로 임포트 됐을 경우) 를 구분하여 app.listen 실행 여부 분기

--- ch5/meadowlark.js
+++ ch5/meadowlark.js
@@ -1,7 +1,18 @@
 const handlers = require('./lib/handlers')
-
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
 const app = express()
+
+// Handlebars 뷰 엔진 설정
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', handlers.home)
 
@@ -10,3 +21,12 @@ app.get('/about', handlers.about)
 app.use(handlers.notFound)
 
 app.use(handlers.serverError)
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log( `Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.' )
+    })
+}
+else {
+    module.exports = app
+}

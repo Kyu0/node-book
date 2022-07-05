bd4344593bc45b38e1f1b6847cb488de75e78404
Kyu0 <efftar@naver.com>
2022-07-05 14:52:35 +0900

    jest 테스트 파일 추가

--- ch5/meadowlark.js
+++ ch5/meadowlark.js
@@ -1,40 +1,12 @@
+const handlers = require('./lib/handlers')
+
 const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
 const app = express()
 
-// Handlebars 뷰 엔진 설정
-app.engine('handlebars', expressHandlebars.engine({
-    defaultLayout: 'main',
-}))
-app.set('view engine', 'handlebars')
+app.get('/', handlers.home)
 
-const port = process.env.PORT || 3000
+app.get('/about', handlers.about)
 
-app.use(express.static(__dirname + '/public'))
+app.use(handlers.notFound)
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
-
-// 404 Page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-
-// 500 Page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
-
-app.listen(port, () => console.log(
-    `Express started on http://lcalhost:${port}\n` +
-    `press Ctrl-C to terminate...`)
-)
+app.use(handlers.serverError)

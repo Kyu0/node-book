2b5c5ab8bde0439680e61081e2a8f716a0070863
Kyu0 <efftar@naver.com>
2022-07-06 20:35:38 +0900

    Chapter 9. 쿠키와 세션

---
+++ ch9/lib/handlers.js
@@ -0,0 +1,41 @@
+const fortune = require('./fortune')
+
+exports.home = (req, res) => res.render('home')
+
+exports.about = (req, res) => res.render('about', { fortune: fortune.getFortune() })
+
+exports.sectionTest = (req, res) => res.render('section-test')
+
+exports.newsletter = (req, res) => {
+    res.render('newsletter', { csrf: 'CSRF token goes here' })
+}
+
+exports.vacationPhotoContest = (req, res) => {
+    res.render('contest/vacation-photo', { csrf: 'CSRF token goes here' })
+}
+
+exports.vacationPhotoContestProcess = (req, res, fields, files) => {
+    console.log('field data: ', fields)
+    console.log('files: ', files)
+    res.redirect(303, '/contest/vacation-photo-thank-you')
+}
+
+exports.api = {
+    newsletterSignup: (req, res) => {
+        console.log(`CSRF token (from hidden form field): ${req.body._csrf}`)
+        console.log(`Name (from visible form field): ${req.body.name}`)
+        console.log(`Email (from visible form field): ${req.body.email}`)
+        res.send({ result: 'success' })
+    }
+}
+
+exports.notFound = (req, res) => res.render('404')
+
+/*
+express는 매개변수가 4 개가 있어야 오류 핸들러로 인식하므로
+next 매개변수는 사용하지 않더라도 없애지 않는다.
+따라서 이 행에 no-unused-vars 규칙을 비활성화한다.
+*/
+/* eslint-disable no-unused-vars */
+exports.serverError = (err, req, res, next) => res.render('500')
+/* eslint-enable no-unused-vars */
---
+++ ch9/meadowlark.js
@@ -0,0 +1,60 @@
+const handlers = require('./lib/handlers')
+const weatherMiddleware = require('./lib/middleware/weather')
+
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
+const app = express()
+
+// Handlebars 뷰 엔진 설정
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        },
+    },
+}))
+
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddleware)
+app.use(bodyParser.json())
+
+app.get('/', handlers.home)
+
+app.get('/about', handlers.about)
+
+app.get('/section-test', handlers.sectionTest)
+
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if (err) return res.status(500).send({ error: err.messge })
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
+
+app.use(handlers.notFound)
+
+app.use(handlers.serverError)
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log( `Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.' )
+    })
+}
+else {
+    module.exports = app
+}

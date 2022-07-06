d505a579ee86305e1c4c9acfcc046c10b1b0d293
Kyu0 <efftar@naver.com>
2022-07-06 16:52:07 +0900

    Chapter 7. 기반 프로젝트 수정

--- ch7/meadowlark.js
+++ ch7/meadowlark.js
@@ -1,6 +1,6 @@
+const handlers = require('./lib/handlers')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-const fortune = require('./lib/fortune')
 const app = express()
 
 // Handlebars 뷰 엔진 설정
@@ -13,28 +13,19 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
-// 404 Page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
-// 500 Page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(
-    `Express started on http://lcalhost:${port}\n` +
-    `press Ctrl-C to terminate...`)
-)
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log( `Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.' )
+    })
+}
+else {
+    module.exports = app
+}
